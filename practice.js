146a51b0414820efaa8566710e12f05b761ea441
Saydullaev Khabibullo <khabibullosaydullaev@gmail.com>
2020-08-17 17:39:37 +0500

    adding practice file

---
+++ practice.js
@@ -0,0 +1,17 @@
+//  Create a function that takes an array of numbers and return "Boom!" if the number 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+// const sevenBoom = (arr) => {
+//     let word = "";
+//     let newArr = arr.join("")
+//     newArr.includes(7) ? (word = "Boom!") : (word = "There is not 7 in array");
+//     return word;
+// };
+// console.log(sevenBoom([12, 23, 34, 45, 6, 97]));
+
+////////////
+// Create a function that takes two dates and returns the number of days between the first and second date.
+// const getDay = (date1, date2) => {
+//     function getDays(date1, date2) {
+//         return new Date(date2 - date1).getDate() - 1;
+//     }
+// };
